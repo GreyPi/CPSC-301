42f82c128b01417fe820edbe47e02387057606dc
GreyPi <47376447+GreyPi@users.noreply.github.com>
2019-03-21 21:32:10 -0700

    Update Assignment4.cpp

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -12,29 +12,74 @@ struct  PERSON
 
 void Display(PERSON DisplayStruct[], int n);
 void FindRichest(PERSON FindStruct[], int n);
-void Deposit(char CustName[], PERSON DepositStruct[], int n);
-void readData(PERSON *&RD);
+void Deposit(PERSON arr[], int N, char custName[], float amount);
+PERSON* readData(int & N);
+
+void printmenu() {
+    cout << "Please enter a choice:" << endl;
+    cout << "1. Display records"<< endl;
+    cout << "2. Deposit funds"<< endl;
+    cout << "3. Find Highest Balance" << endl;
+    cout << "4. Update records" << endl;
+    cout << "5. Exit the program" << endl;
+}
 
 int main()
 {
-	PERSON * readData(int & N);
+	int N = 0;
+	char custName[20];
+	float amt = 0;
+	PERSON* P = new PERSON[N];
+	P = readData(N);
 	
-	cout << * readData.Name << endl;
-	//Display(P, N);
-	//FindRichest(P, N);
-	//Deposit(customer, P, N);
-			
+	{
+    int choice;
+    do
+    {
+        printmenu();
+        cin >> choice;
+        switch(choice)
+        {
+            case 1:
+                Display(P, N); // Act on display
+                break;
+
+            case 2:
+                Deposit(P, N, custName, amt); // Act on deposit
+
+                break;
+
+            case 3:
+                FindRichest(P, N); // Act highest balance
+                break;
+
+            case 4:
+                // Act on update records
+                break;
+
+            case 5:
+                // Must call update records here before exiting the program
+                break;
+
+            default:
+                cout << "Invalid entry" << endl;
+                break;
+        }
+        cout << endl;
+   } while(choice != 5);
+      	
 	return 0;
 }
+}
+}
 
-void readData(PERSON *&RD, int n)
+PERSON* readData(int & N)
 {
 	ifstream inData;
 	inData.open("data.txt");
 	char first[20];
 	char last[20];
 	
-	int N = 0;
 	string numRecords;
 	while (getline(inData, numRecords))
 	{
@@ -42,22 +87,35 @@ void readData(PERSON *&RD, int n)
 	}
 	inData.close();
 	
+	PERSON * RD = new PERSON[N];
+	
 	inData.open("data.txt");
 		while(!inData.eof())
 		{
-			for (int i =0; i < n; i++)
+			for (int i =0; i < N; i++)
 				{
 					inData >>  RD[i].Name;
 					strcpy(first, RD[i].Name);
+					if (strlen(first) > 9)
+					{
+					cout << "The name is too long";
+					}	
+					else
+					
 					inData >> RD[i].Name;
 					strcpy(last, RD[i].Name);
+					if (strlen(last) > 10)
+					{
+					cout << "The name is too long";
+					}
+					else
 					strcpy(RD[i].Name, first);
 					strcat(RD[i].Name, " ");
 					strcat(RD[i].Name, last);
 					inData >> RD[i].Balance;
 				}
 		}
-	
+	return RD;
 }
 
 void Display(PERSON DisplayStruct[], int n)
@@ -87,36 +145,36 @@ void FindRichest(PERSON FindStruct[], int n)
 	cout << endl << endl;
 }
 
-void Deposit(char CustName[], PERSON DepositStruct[], int n)
+void Deposit(PERSON arr[], int N, char custName[], float amount)
 {
 	float value = 0;
 	float total = 0;
 	string recs;
 	cout << "Enter your full name to deposit money: ";
-	cin.getline(CustName, 20);
+	cin.getline(custName, 20);
 	
-	for (int i=0; i<n; i++)
+	for (int i=0; i<N; i++)
 		{
-			if (strcmp(CustName, DepositStruct[i].Name) == 0)
+			if (strcmp(custName, arr[i].Name) == 0)
 			{
-				cout << DepositStruct[i].Name << 
+				cout << arr[i].Name << 
 				", how much would you like to deposit? ";
 				cin >> value;
-				total = value + DepositStruct[i].Balance;
-				DepositStruct[i].Balance += value;
+				total = value + arr[i].Balance;
+				arr[i].Balance += value;
 			}
 		}
 				ifstream inData;
-				inData.open("data.txt");
+				inData.open("OG.txt");
 				ofstream outData;
-				outData.open("data.txt");
+				outData.open("OG.txt");
 				while (!inData.eof())
 				{
-					for (int k = 0; k < n; k++)
+					for (int k = 0; k < N; k++)
 					{
-						outData << DepositStruct[k].Name;
+						outData << arr[k].Name;
 						outData << "  ";
-						outData << setprecision (2) << fixed << DepositStruct[k].Balance;
+						outData << setprecision (2) << fixed << arr[k].Balance;
 						outData << endl;
 						getline(inData, recs);
 					}
