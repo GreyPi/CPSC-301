daf6cd50aad85468de2e782123516c23c577eb37
GreyPi <47376447+GreyPi@users.noreply.github.com>
2019-04-16 21:47:34 -0700

    Add files via upload

---
+++ checkout.cpp
@@ -0,0 +1,159 @@
+//Gregory Pierot
+//Section #2
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "person.cpp"
+#include "book.cpp"
+
+using namespace std;
+
+void printMenu() {
+    cout << "----------Library Book Rental System----------" << endl;
+    cout << "1.  Book checkout" << endl;
+    cout << "2.  Book return" << endl;
+    cout << "3.  View all available books" << endl;
+    cout << "4.  View all outstanding rentals" << endl;
+    cout << "5.  View outstanding rentals for a cardholder" << endl;
+    cout << "6.  Open new library card" << endl;
+    cout << "7.  Close library card" << endl;
+    cout << "8.  Exit system" << endl;
+    cout << "Please enter a choice: ";
+}
+
+/* You are not obligated to use these function declarations - 
+ * they're just given as examples*/
+void readBooks(vector<Book *> & myBooks) 
+{
+	int id = 0;
+	string title, author, category;
+	string line;
+	
+    ifstream inData;
+    inData.open("books.txt");
+    while (inData >> id)
+    {
+		getline(inData, line);
+		inData >> title;
+		getline(inData, line);
+		inData >> author;
+		getline(inData, line);
+		inData >> category;
+		getline(inData, line);
+		Book * book = new Book(id,title,author,category);
+		myBooks.push_back(book);
+	}
+  
+}
+
+int readPersons(vector<Person *> & myCardholders) 
+{
+	int cardNo; 
+	bool act;
+	string fName, lName, line;
+	
+    ifstream inData;
+    inData.open("persons.txt");
+    while (inData >> cardNo)
+    {
+		inData >> cardNo;
+		inData >> act;
+		inData >> fName;
+		inData >> lName;
+		getline(inData, line);
+		Person * person = new Person(cardNo, act, fName, lName);
+		myCardholders.push_back(person);
+	}
+    return 0;
+}
+
+void readRentals(vector<Book *> & myBooks, vector<Person *> myCardholders) 
+{
+	int bkId, crdId;
+	string line;
+    ifstream inData;
+    inData.open("persons.txt");
+	while (inData >> bkId)
+	{
+		inData >> crdId;
+		getline(inData, line);
+		/*for (unsigned int i = 0; i < myBooks.size(); i++)
+		{
+			if (bkId == myBooks.at(i)->getId())
+			{
+				
+			}
+		}*/
+		// Unfinished
+		
+	}
+    
+}
+
+void openCard(vector<Person *> & myCardholders, int nextID) {
+    return;
+}
+
+Book * searchBook(vector<Book *> myBooks, int id) {
+    return nullptr;
+}
+
+
+int main()
+{
+    vector<Book *> books;
+    vector<Person *> cardholders;
+    readBooks(books);
+    readPersons(cardholders);
+    readRentals(books, cardholders);
+    
+    int choice;
+    do
+    {
+        // If you use cin anywhere, don't forget that you have to handle the <ENTER> key that 
+        // the user pressed when entering a menu option. This is still in the input stream.
+        printMenu();
+        cin >> choice;
+        switch(choice)
+        {
+            case 1:
+                // Book checkout
+                break;
+
+            case 2:
+                // Book return
+                break;
+
+            case 3:
+                // View all available books
+                break;
+
+            case 4:
+                // View all outstanding rentals
+                break;
+
+            case 5:
+                // View outstanding rentals for a cardholder
+                break;
+
+            case 6:
+                // Open new library card
+                break;
+
+            case 7:
+                // Close library card
+                break;
+                
+            case 8:
+                // Must update records in files here before exiting the program
+                break;
+
+            default:
+                cout << "Invalid entry" << endl;
+                break;
+        }
+        cout << endl;
+   } while(choice != 8);
+      return 0;
+}
